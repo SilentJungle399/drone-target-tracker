d7da886739a86c634a8ec505057355fb46336ff7
SilentJungle399 <vardhan.harsh4041@gmail.com>
2026-03-14 04:45:54 +0530

    updated constants

--- autonomous/config.py
+++ autonomous/config.py
@@ -2,14 +2,14 @@
 # MISSION PARAMETERS (EDIT THESE)
 # ==========================================================
 
-TARGET_ALTITUDE = 2.0                 # meters
+TARGET_ALTITUDE = 1.5                # meters
 WAYPOINT_ACCEPTANCE_RADIUS = 2.5      # meters
 
 CENTER_TOLERANCE = 40                 # pixels
 
-CORRECTION_SPEED = 0.7               # m/s velocity for centering corrections
+CORRECTION_SPEED = 0.4               # m/s velocity for centering corrections
 
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 640
 
-YOLO_CONFIDENCE = 0.8
+YOLO_CONFIDENCE = 0.6
